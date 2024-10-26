4a7223da4efb1317ec0de086365cb54ad6e4bef7
LetsTray <letstraey@gmail.com>
2024-10-26 18:31:55 +0700

    add pages

---
+++ frontend/src/Layout/RootLayout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Header from '../Components/Header'
+import { Outlet } from 'react-router-dom'
+
+const RootLayout = () => {
+  return (
+    <div>
+        <Header/>
+        <Navbar/>
+        <div>
+            <Outlet/>
+        </div>
+      
+    </div>
+  )
+}
+
+export default RootLayout
---
+++ frontend/src/Pages/NewArrivals.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NewArrivals = () => {
+  return (
+    <div className=" w-full">
+        <h1 className=" txt-5xl flex justify-center uppercase p-44">
+            <b>New Arrivals Page</b>
+        </h1>
+    </div>
+  )
+}
+
+export default NewArrivals
