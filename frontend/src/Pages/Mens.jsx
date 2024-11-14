a7489543495496229d5d24608116aa495bdf0c93
aufariss <abdulfarisaufar@gmail.com>
2024-11-14 13:41:17 +0700

    update code

--- frontend/src/Pages/Mens.jsx
+++ frontend/src/Pages/Mens.jsx
@@ -2,11 +2,7 @@ import React from "react";
 
 const Mens = () => {
   return (
-    <div className=" w-full">
-      <h1 className=" txt-5xl flex justify-center uppercase p-44">
-        <b>Mens Page</b>
-      </h1>
-    </div>
+
   );
 }
 
