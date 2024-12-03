193cf051e14706bed6e01fd02f48071cf1437670
zaky ramadhan <zakyramadhanxstudy@gmail.com>
2024-12-03 22:52:01 +0700

    commit seadanya

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -26,10 +26,16 @@ export const login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const token = await loginUser(email, password);
+    const { user, token } = await loginUser(email, password);
 
     res.status(200).json({
       message: "User logged in successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+      },
       token,
     });
   } catch (error) {
--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -1,16 +1,21 @@
 import bcrypt from "bcryptjs";
-import { generateToken } from "../utils/jwt.js"; // Impor fungsi spesifik
+import { generateToken } from "../utils/jwt.js";
 import User from "../models/user.js";
 
 // Register a new user
 export const registerUser = async (userData) => {
-  const { email, password, name } = userData;
+  const { email, password, name, phone } = userData;
 
   const existingUser = await User.findOne({ email });
   if (existingUser) throw new Error("User already exists");
 
   const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = new User({ email, password: hashedPassword, name });
+  const newUser = new User({
+    email,
+    password: hashedPassword,
+    name,
+    phone,
+  });
   await newUser.save();
 
   return newUser;
@@ -27,5 +32,6 @@ export const loginUser = async (email, password) => {
   const token = generateToken({ id: user._id }, process.env.JWT_SECRET, {
     expiresIn: "1h",
   });
+
   return { user, token };
 };
