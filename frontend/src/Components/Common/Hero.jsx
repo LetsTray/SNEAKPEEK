bdcbe4ee3a35cac45a7cdd64d6ba772286c27722
zaky ramadhan <zakyramadhanxstudy@gmail.com>
2024-12-04 14:34:32 +0700

    fix hero image

--- frontend/src/Components/Common/Hero.jsx
+++ frontend/src/Components/Common/Hero.jsx
@@ -1,18 +1,21 @@
 import React from "react";
-import "../../assets/images/hero.jpg";
+import hero from "../../assets/images/hero.jpg"; // Make sure the image path is correct
 
 const Hero = () => {
   return (
     <div>
-      <div className=" w-full bg-hero-pattern min-h-screen bg-cover bg-center text-white flex items-center justify-start">
-        <div className=" ml-10 text-6xl">
-          <p className=" pb-3">
+      <div
+        className="w-full min-h-screen bg-cover bg-center text-white flex items-center justify-start"
+        style={{ backgroundImage: `url(${hero})` }} // Set background image via inline style
+      >
+        <div className="ml-10 text-6xl">
+          <p className="pb-3">
             <b>Your</b>
           </p>
-          <p className=" pb-3">
+          <p className="pb-3">
             <b>Gateway to</b>
           </p>
-          <p className=" pb-3">
+          <p className="pb-3">
             <b>Exclusive</b>
           </p>
           <p>
@@ -21,21 +24,21 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className=" w-full flex items-center justify-center border border-black h-32 bg-black">
-        <div className=" flex text-6xl">
-          <p className=" text-neonGreen">
+      <div className="w-full flex items-center justify-center border border-black h-32 bg-black">
+        <div className="flex text-6xl">
+          <p className="text-neonGreen">
             <b>LOREM IPSUM</b>
           </p>
-          <p className=" text-white">
+          <p className="text-white">
             <b>//</b>
           </p>
-          <p className=" text-gray-400">
+          <p className="text-gray-400">
             <b>LOREM IPSUM</b>
           </p>
-          <p className=" text-white">
+          <p className="text-white">
             <b>//</b>
           </p>
-          <p className=" text-white">
+          <p className="text-white">
             <b>LOREM IPSUM</b>
           </p>
         </div>
